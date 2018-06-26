cf879a487cc24aa997d65ef6d8e7f27256fd2e9b
pgarces <gspablo@gmail.com>
2018-06-26 09:21:14 +0200

    Updated generate including composer into container and added composer.phar to git ignore

--- generate.sh
+++ generate.sh
@@ -4,8 +4,10 @@
 docker-compose down
 docker-compose up -d --build woocommerce-test
 docker-compose up -d selenium
+docker-compose exec woocommerce-test curl -s https://getcomposer.org/installer | php
+docker-compose exec woocommerce-test ./composer.phar install
+
 npm install
-composer install
 node_modules/.bin/grunt
 
 # Time to boot and install woocommerce
@@ -13,6 +15,7 @@ sleep 30
 set -e
 
 # Run test
+
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-basic
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-install
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-buy-unregistered
