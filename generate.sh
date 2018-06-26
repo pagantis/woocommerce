2ccfa52cff08e7a79db550fabfad03f98b0bcd6c
pgarces <gspablo@gmail.com>
2018-06-26 09:30:11 +0200

    Updated preference of action in generate.sh

--- generate.sh
+++ generate.sh
@@ -4,18 +4,16 @@
 docker-compose down
 docker-compose up -d --build woocommerce-test
 docker-compose up -d selenium
+npm install
+docker-compose exec woocommerce-test node_modules/.bin/grunt
 docker-compose exec woocommerce-test curl -s https://getcomposer.org/installer | php
 docker-compose exec woocommerce-test ./composer.phar install
-
-npm install
-node_modules/.bin/grunt
 
 # Time to boot and install woocommerce
 sleep 30
 set -e
 
 # Run test
-
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-basic
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-install
 docker-compose exec woocommerce-test vendor/bin/phpunit --group woocommerce3-buy-unregistered
