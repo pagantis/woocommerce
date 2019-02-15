87a464d828eb0258fe63336e87c8aaff5da37486
pgarces <gspablo@gmail.com>
2019-02-15 13:02:53 +0100

    Update panel options

--- includes/settings-paylater.php
+++ includes/settings-paylater.php
@@ -13,27 +13,19 @@ return array(
         'type'        => 'checkbox',
         'default'     => 'no'
     ),
-    'public_key' => array(
+    'pmt_public_key' => array(
         'title'       => __('Public Key', 'paylater'),
         'type'        => 'text',
         'description' => __('MANDATORY. You can get in your pagamastarde profile', 'paylater')
     ),
-    'secret_key' => array(
+    'pmt_private_key' => array(
         'title'       => __('Secret Key', 'paylater'),
         'type'        => 'text',
         'description' => __('MANDATORY. You can get in your pagamastarde profile', 'paylater')
     ),
     'simulator' => array(
-        'title'       => __('Product simulator', 'paylater'),
-        'type'        => 'select',
-        'description' => __('Include simulator in product page', 'paylater'),
-        'default'     => '6',
-        'desc_tip'    => true,
-        'options'     => array(
-            '0'       => __('No', 'paylater'),
-            'pmtSDK.simulator.types.SIMPLE'     => __('Simple', 'paylater'),
-            'pmtSDK.simulator.types.SELECTABLE' => __('Selectable', 'paylater'),
-            'pmtSDK.simulator.types.TEXT'       => __('Descriptive text', 'paylater'),
-        )
+        'title'       => __('Simulator', 'paylater'),
+        'type'        => 'checkbox',
+        'default'     => 'yes'
     )
 );
