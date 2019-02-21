a25b0dc2fa511320aa26ce21d930809b5fe51b19
pgarces <gspablo@gmail.com>
2019-02-21 17:37:20 +0100

    updated product simulator to js

--- templates/product_simulator.php
+++ templates/product_simulator.php
@@ -3,18 +3,31 @@
 
     function loadSimulator()
     {
-        var positionSelector = '<? echo $pmtCSSSelector;?>';
-
+        var positionSelector = '<? echo $positionSelector;?>';
         if (positionSelector === 'default') {
             positionSelector = '.PmtSimulator';
         }
 
+        var priceSelector = '<? echo $priceSelector;?>';
+        if (priceSelector === 'default') {
+            priceSelector = 'div.summary.entry-summary span.woocommerce-Price-amount.amount';
+        }
+
+        var quantitySelector = '<? echo $quantitySelector;?>';
+        if (quantitySelector === 'default') {
+            quantitySelector = 'div.quantity>input';
+        }
+
+        console.log(positionSelector);
+        console.log(priceSelector);
+        console.log(quantitySelector);
         if (typeof pmtSDK != 'undefined') {
             pmtSDK.simulator.init({
                 publicKey: '<?php echo $public_key; ?>',
-                selector: positionSelector,
                 type: <?php echo $simulator_type; ?>,
-                totalAmount:<?php echo $totalAmount; ?>
+                selector: positionSelector,
+                itemQuantitySelector: quantitySelector,
+                itemAmountSelector: priceSelector
             });
             clearInterval(simulatorId);
         }
