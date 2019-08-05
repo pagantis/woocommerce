93cd8a86a6e505aed49be1cacb850721ec0fb522
pgarces <gspablo@gmail.com>
2019-08-05 17:27:39 +0200

    Tests. Adding trace log

--- test/selenium/buy/AbstractBuy.php
+++ test/selenium/buy/AbstractBuy.php
@@ -447,9 +447,12 @@ abstract class AbstractBuy extends PagantisWoocommerceTest
         $dateFrom = date("Ymd", strtotime("-1 day"));
         $dateTo = date("Ymd", strtotime("+1 day"));
         $apiUrl = $this->woocommerceUrl.self::API_FOLDER.$this->configuration['secretKey']."/$dateFrom/$dateTo";
+        echo $apiUrl;
         $response = Request::get($apiUrl)->expects('json')->send();
-        $this->assertNotEmpty($response->body);
-        $this->assertEquals(1, count($response->body), "PR63=>".$apiUrl." = ".count($response->body));
+        $this->assertNotEmpty($response->body->message);
+        echo "<pre>";
+        var_dump($response->body->message);
+        $this->assertEquals(1, count($response->body->message), "PR63=>".$apiUrl." = ".count($response->body));
     }
 
     /**
