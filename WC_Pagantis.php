b3cbd4ff9757a7ab746c747fd98941e4a76d13c1
rmarinleal <raul.marin.leal@gmail.com>
2019-10-02 10:52:13 +0200

    Update WC_Pagantis.php

--- WC_Pagantis.php
+++ WC_Pagantis.php
@@ -47,7 +47,7 @@ class WcPagantis
        'PAGANTIS_DISPLAY_MIN_AMOUNT'=>1,
        'PAGANTIS_URL_OK'=>'',
        'PAGANTIS_URL_KO'=>'',
-       'PAGANTIS_ALLOWED_COUNTRIES' => 'a:2:{i:0;s:2:"es";i:1;s:2:"it";}',
+       'PAGANTIS_ALLOWED_COUNTRIES' => 'a:3:{i:0;s:2:"es";i:1;s:2:"it";i:2;s:2:"fr";}',
        'PAGANTIS_PROMOTION_EXTRA' => '<p>Finance this product <span class="pmt-no-interest">without interest!</span></p>',
        'PAGANTIS_SIMULATOR_THOUSANDS_SEPARATOR' => '.',
        'PAGANTIS_SIMULATOR_DECIMAL_SEPARATOR' => ','
