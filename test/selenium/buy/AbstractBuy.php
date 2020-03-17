5d2236db8126669e8cca0fce218e3391f86f1bef
pgarces <gspablo@gmail.com>
2020-03-17 15:50:45 +0100

    Test. Remove debug

--- test/selenium/buy/AbstractBuy.php
+++ test/selenium/buy/AbstractBuy.php
@@ -314,10 +314,6 @@ abstract class AbstractBuy extends PagantisWoocommerceTest
      */
     public function verifyOrderInformation()
     {
-        echo $this->webDriver->getCurrentURL();
-
-        echo $this->webDriver->getPageSource();
-
         $messageElementSearch = WebDriverBy::className('entry-title');
         $condition = WebDriverExpectedCondition::visibilityOfElementLocated($messageElementSearch);
         $this->waitUntil($condition);
