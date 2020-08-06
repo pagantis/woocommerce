26ea406a4e5e637d38e87ddb52c5382edaf2ac7f
pgarces <gspablo@gmail.com>
2020-08-06 08:01:18 +0200

    Update version

--- WC_Pagantis.php
+++ WC_Pagantis.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pagantis
  * Plugin URI: http://www.pagantis.com/
  * Description: Financiar con Pagantis
- * Version: 8.6.1
+ * Version: 8.6.2
  * Author: Pagantis
  *
  * Text Domain: pagantis
