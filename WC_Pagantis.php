d1379a980fbc21deb79e25bd87490d73daeaebe6
tmeliotpg <tmeliot@pagantis.com>
2020-10-01 14:19:22 +0200

    updated version

--- WC_Pagantis.php
+++ WC_Pagantis.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pagantis
  * Plugin URI: http://www.pagantis.com/
  * Description: Financiar con Pagantis
- * Version: 8.6.13
+ * Version: 8.6.14
  * Author: Pagantis
  *
  * Text Domain: pagantis
